6d84bed3ad604364317676d98aee4422f3551c4a
Shakiba448 <shakiba448@gmail.com>
2021-08-19 20:21:28 +0600

    TS Crash Course

--- dist/index.js
+++ dist/index.js
@@ -3,3 +3,8 @@
 let id = 5;
 let company = "Shakib";
 let isPublished = true;
+let x = "Hello world";
+let ids = [1, 2, 2, 3, 4, 5];
+let arr = [1, 2, 3, true, "shakib"];
+// Tuple
+let person = [1, "shakib", false];
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,12 @@
 let id: number = 5;
 let company: string = "Shakib";
 let isPublished: boolean = true;
+let x: any = "Hello world";
+
+let ids: number[] = [1, 2, 2, 3, 4, 5];
+
+let arr: any[] = [1, 2, 3, true, "shakib"];
+
+// Tuple
+
+let person: [number, string, boolean] = [1, "shakib", false];
